d162d3b0354ca143c406c83a32e6ffff024e3c97
thejayduck <ardafevzi.armutcu@gmail.com>
2021-11-20 20:53:59 +0300

    General Improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,16 +20,12 @@ const skills = [
     icon: "devicon-nextjs-plain",
   },
   {
-    title: "MongoDB",
-    icon: "devicon-mongodb-plain-wordmark",
-  },
-  {
     title: "JavaScript",
     icon: "devicon-javascript-plain",
   },
   {
-    title: "PHP",
-    icon: "devicon-php-plain",
+    title: "Rust",
+    icon: "devicon-rust-plain",
   },
   {
     title: "HTML",
@@ -42,7 +38,19 @@ const skills = [
   {
     title: "SASS",
     icon: "devicon-sass-original",
-  }
+  },
+  {
+    title: "PHP",
+    icon: "devicon-php-plain",
+  },
+  {
+    title: "Postgresql",
+    icon: "devicon-postgresql-plain",
+  },
+  {
+    title: "MongoDB",
+    icon: "devicon-mongodb-plain-wordmark",
+  },
 ];
 
 const tools = [
@@ -84,12 +92,15 @@ export default function Home({ data }: HomeProps) {
           <img src="profileLarge.png" width={256} />
           <ul>
             <SocialItem icon="bx bxl-github" title="Github" url="https://github.com/thejayduck" />
-            <SocialItem icon="bx bxl-twitter" title="Twitter" url="https://github.com/thejayduck" />
+            <SocialItem icon="bx bxl-twitter" title="Twitter" url="https://twitter.com/thejayduck" />
             <SocialItem icon="bx bxs-envelope" title="Email" url="mailto:ardafevzi.armutcu@gmail.com" />
+            <SocialItem icon="bx bxs-shopping-bag" title="Itch.io" url="https://thejayduck.itch.io/" />
+            <SocialItem icon="bx bxl-instagram-alt" title="Instagram" url="https://www.instagram.com/ardafevzi.armutcu/" />
+
           </ul>
         </div>        
         <div className={`${styles.details}`}>
-          <div className={`cardItem ${styles.about}`}>
+          <div className={`cardItem ${styles.panel}`}>
             <div className={styles.introduction}>
               <h1>
                 Hi there
@@ -104,6 +115,8 @@ export default function Home({ data }: HomeProps) {
               </p>
             </div>
             <br/>
+          </div>
+          <div className={`cardItem ${styles.panel}`}>
             <h1> Skill Set 🔧 </h1>
             <Subtitle text="Languages"/>           
             <div className={`${styles.skills} flex`}>
@@ -118,10 +131,8 @@ export default function Home({ data }: HomeProps) {
               )}   
             </div>   
           </div>
-          <div className={`cardItem ${styles.worksWrap}`}>
-            <h1>
-                Recent Projects
-            </h1>
+          <div className={`cardItem ${styles.panel}`}>
+            <h1> Recent Projects 💻 </h1>
             <div className={`flex ${styles.works}`}>
               {
                 data.map(q => <Project key={q.title} data={q} />)
