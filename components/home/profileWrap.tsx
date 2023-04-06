5165e3d4e4fcecfe4b184b0124991dbf3e2287a5
thejayduck <ardafevzi.armutcu@gmail.com>
2023-04-06 17:19:09 +0300

    Small fix before (possible) rewrite

--- components/home/profileWrap.tsx
+++ components/home/profileWrap.tsx
@@ -9,13 +9,14 @@ export default function ProfileWrap() {
       <div className={styles.navigation}>
         <ul className={`${styles.pageList}`}>
           <SocialItem icon="bx bxs-box" label="blog" title="Blog" href="/blog" newPage={false} />
+          <SocialItem icon="bx bxs-detail" label="resume" title="Resume" href="/resume" newPage={false} />
           <SocialItem icon="bx bxs-image" label="gallery" title="Gallery" href="/gallery" newPage={false} />
         </ul>
         <ul className={`${styles.socialList}`}>
           <SocialItem icon="bx bxl-github" label="github" title="Github" href="https://github.com/thejayduck" />
           <SocialItem icon="bx bxs-store" label="itch.io" title="Itch.io" href="https://thejayduck.itch.io/" />
           {/* <SocialItem icon="bx bxl-deviantart" label="deviantart" href="https://www.deviantart.com/thejayduck/" /> */}
-          <SocialItem icon="bx bxl-instagram-alt" label="instagram" title="hey" href="https://www.instagram.com/ardafevzi.armutcu/" />
+          <SocialItem icon="bx bxl-instagram-alt" label="instagram" title="Instagram" href="https://www.instagram.com/ardafevzi.armutcu/" />
           <SocialItem icon="bx bxl-gmail" label="gmail" title="Gmail" href="mailto:ardafevzi.armutcu@gmail.com" />
         </ul>
       </div>
