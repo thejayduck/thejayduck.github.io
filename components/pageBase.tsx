e23f38b96332467418206a8e767438ddcaca0ecf
thejayduck <ardafevzi.armutcu@gmail.com>
2024-02-26 16:51:15 +0100

    Adds notification to a custom streaming website.

--- components/pageBase.tsx
+++ components/pageBase.tsx
@@ -2,11 +2,13 @@
 import styles from "../styles/PageBase.module.scss";
 
 import ScrollManager from "./scrollManager";
+import StreamNotification from "./streamNotification";
 
 export default function PageBase({ children }) {
   return (
     <main className={styles.container}>
       <ScrollManager />
+      <StreamNotification />
       {children}
     </main>
   );
---
+++ components/streamNotification.tsx
@@ -0,0 +1,44 @@
+import styles from "../styles/components/StreamNotification.module.scss";
+
+import Link from "next/link";
+
+import { useEffect, useState } from "react";
+
+export default function StreamNotification() {
+  const [streamStatus, setStreamStatus] = useState(false);
+  const [streamTitle, setStreamTitle] = useState(null);
+
+  useEffect(() => {
+    const fetchStreamStatus = async () => {
+      try {
+        const response = await fetch(
+          "http://livestream.ardarmutcu.com/status.php"
+        );
+        if (!response.ok) {
+          throw new Error("Network responded with an error.");
+        }
+        const data = await response.json();
+
+        setStreamStatus(data.is_active);
+        setStreamTitle(data.stream_title);
+      } catch (error) {
+        console.error("Error fetching status:", error);
+      }
+    };
+    fetchStreamStatus();
+  }, []);
+
+  return (
+    <>
+      {streamStatus && (
+        <div className={styles.streamNotification}>
+          <Link href="https://livestream.ardarmutcu.com" passHref>
+            <span>
+              ⏺ Live Now: {streamTitle} - Click here to join the stream!
+            </span>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+}
