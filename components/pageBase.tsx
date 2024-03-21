8596e5cc116a0f67367b6d132afb07fc79af07c2
thejayduck <ardafevzi.armutcu@gmail.com>
2024-03-21 23:00:34 +0100

    Temporarily Disables a Stream Notifications

--- components/pageBase.tsx
+++ components/pageBase.tsx
@@ -2,13 +2,13 @@
 import styles from "../styles/PageBase.module.scss";
 
 import ScrollManager from "./scrollManager";
-import StreamNotification from "./streamNotification";
+// import StreamNotification from "./streamNotification";
 
 export default function PageBase({ children }) {
   return (
     <main className={styles.container}>
       <ScrollManager />
-      <StreamNotification />
+      {/* <StreamNotification /> */}
       {children}
     </main>
   );
