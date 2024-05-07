d7239a4a7de9a4b358d57f31340f044553b7b4bb
thejayduck <ardafevzi.armutcu@gmail.com>
2024-05-07 10:55:32 +0200

    Removes unused lines

--- components/pageBase.tsx
+++ components/pageBase.tsx
@@ -2,13 +2,11 @@
 import styles from "../styles/PageBase.module.scss";
 
 import ScrollManager from "./scrollManager";
-// import StreamNotification from "./streamNotification";
 
 export default function PageBase({ children }) {
   return (
     <main className={styles.container}>
       <ScrollManager />
-      {/* <StreamNotification /> */}
       {children}
     </main>
   );
