b664627061ad68119b10b31fdd84d7eb963bbaa0
thejayduck <ardafevzi.armutcu@gmail.com>
2024-05-10 15:51:21 +0200

    Update imageShimmer.tsx

--- components/imageShimmer.tsx
+++ components/imageShimmer.tsx
@@ -1,3 +1,5 @@
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+
 export const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
@@ -16,3 +18,10 @@ export const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
+
+export const placeholderImage = (w: number, h: number) =>
+  `data:image/svg+xml;base64,${toBase64(shimmer(w, h))}` as PlaceholderValue;
+
+//  placeholder={`data:image/svg+xml;base64,${toBase64(
+//             shimmer(selectedImage.width, selectedImage.height)
+//           )}`}
