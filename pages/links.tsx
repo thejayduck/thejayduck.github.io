c470a1cdeb6fabd1cc00ecfd1117082e86b0a9d5
thejayduck <ardafevzi.armutcu@gmail.com>
2025-02-13 10:28:07 +0100

    Updates links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -23,62 +23,65 @@ export default function Links() {
       </Head>
 
       <PageBase>
-        <section className={`${styles.panelsWrap}`}>
-          <CardPanel
-            title={
-              <>
-                <Image
-                  className={styles.picture}
-                  alt="Profile Picture"
-                  src="https://utfs.io/a/hj5xs4m6pg/qeLXJUQ9GpPCSifPjnRJBnDwh9l4xkuN2T7qp3LbvZdHVfIi"
-                  priority
-                  placeholder={placeholderImage(256, 446)}
-                  width={96}
-                  height={128}
-                  quality={85}
-                />
-                <span>Find Me On</span>
-              </>
-            }
-          >
-            <ul className={`${styles.linkList}`}>
-              <Button // linkedin
-                icon="bx bxl-linkedin-square"
-                label="linkedin"
-                title="LinkedIn"
-                href="https://linkedin.com/in/armutcu"
-                newPage={true}
-              />
-              <Button // github
-                icon="bx bxl-github"
-                label="github"
-                title="Github"
-                href="https://github.com/thejayduck/"
-                newPage={true}
-              />
-              <Button // itchio
-                icon="bx bxs-store"
-                label="itchio"
-                title="Itch.io"
-                href="https://thejayduck.itch.io/"
-                newPage={true}
-              />
-              <Button // deviantart
-                icon="bx bxl-deviantart"
-                label="deviantart"
-                title="DeviantArt"
-                href="https://www.deviantart.com/thejayduck"
-                newPage={true}
-              />
-              <Button // instagram
-                icon="bx bxl-instagram-alt"
-                label="instagram"
-                title="Instagram"
-                href="https://www.instagram.com/ardafevzi.armutcu/"
-                newPage={true}
-              />
-            </ul>
-          </CardPanel>
+        <section className={`${styles.panelsWrap} cardItem`}>
+          <div className={styles.header}>
+            <Image
+              className={styles.picture}
+              alt="Profile Picture"
+              src="https://utfs.io/a/hj5xs4m6pg/qeLXJUQ9GpPCSifPjnRJBnDwh9l4xkuN2T7qp3LbvZdHVfIi"
+              priority
+              placeholder={placeholderImage(96, 128)}
+              width={96}
+              height={128}
+              quality={85}
+            />
+            <h1>Find Me On</h1>
+          </div>
+          <ul className={`${styles.linkList}`}>
+            <Button // linkedin
+              icon="bx bxl-linkedin-square"
+              label="linkedin"
+              title="LinkedIn"
+              href="https://linkedin.com/in/armutcu"
+              newPage={true}
+            />
+            <Button // github
+              icon="bx bxl-github"
+              label="github"
+              title="Github"
+              href="https://github.com/thejayduck/"
+              newPage={true}
+            />
+            <Button // itchio
+              icon="bx bxs-store"
+              label="itchio"
+              title="Itch.io"
+              href="https://thejayduck.itch.io/"
+              newPage={true}
+            />
+            <Button // deviantart
+              icon="bx bxl-deviantart"
+              label="deviantart"
+              title="DeviantArt"
+              href="https://www.deviantart.com/thejayduck"
+              newPage={true}
+            />
+            <Button // cara
+              icon="bx bxs-copyright"
+              label="cara"
+              title="Cara"
+              href="https://cara.app/thejayduck"
+              newPage={true}
+            />
+            <Button // instagram
+              icon="bx bxl-instagram-alt"
+              label="instagram"
+              title="Instagram"
+              href="https://www.instagram.com/ardafevzi.armutcu/"
+              newPage={true}
+            />
+          </ul>
+          <span>This page is work in progress!</span>
         </section>
       </PageBase>
     </>
