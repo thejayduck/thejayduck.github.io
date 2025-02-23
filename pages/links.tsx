d0ab3541747bd2f60aad2bade058a98cafb271a8
thejayduck <ardafevzi.armutcu@gmail.com>
2025-02-23 12:52:08 +0100

    Improves links page, implements toasts

--- pages/links.tsx
+++ pages/links.tsx
@@ -4,11 +4,59 @@ import Head from "next/head";
 import Image from "next/image";
 
 import Button from "../components/button";
-import CardPanel from "../components/cardPanel";
 import { placeholderImage } from "../components/imageShimmer";
 import PageBase from "../components/pageBase";
+import { useToast } from "../components/toashHandler";
+
+interface SocialLink {
+  icon: string;
+  label: string;
+  title: string;
+  href: string;
+}
+
+const socialLinks: SocialLink[] = [
+  {
+    icon: "bx bxl-linkedin-square",
+    label: "linkedin",
+    title: "LinkedIn",
+    href: "https://linkedin.com/in/armutcu",
+  },
+  {
+    icon: "bx bxl-github",
+    label: "github",
+    title: "Github",
+    href: "https://github.com/thejayduck/",
+  },
+  {
+    icon: "bx bxs-store",
+    label: "itchio",
+    title: "Itch.io",
+    href: "https://thejayduck.itch.io/",
+  },
+  {
+    icon: "bx bxs-copyright",
+    label: "cara",
+    title: "Cara",
+    href: "https://cara.app/thejayduck",
+  },
+  {
+    icon: "bx bxl-deviantart",
+    label: "deviantart",
+    title: "DeviantArt",
+    href: "https://www.deviantart.com/thejayduck",
+  },
+  {
+    icon: "bx bxl-instagram-alt",
+    label: "instagram",
+    title: "Instagram",
+    href: "https://www.instagram.com/ardafevzi.armutcu/",
+  },
+];
 
 export default function Links() {
+  const { showToast } = useToast();
+
   return (
     <>
       <Head>
@@ -38,60 +86,76 @@ export default function Links() {
             <h1>@thejayduck</h1>
           </div>
           <ul className={`${styles.linkList}`}>
-            <Button
-              icon="bx bx-link"
-              label="copy"
-              title="Share Page"
-              newPage={false}
-              onClick={() => {
-                navigator.clipboard.writeText(window.location.href);
-                alert("Link copied to clipboard!"); // TODO make a custom notification like system
-              }}
-            />
-            <Button // linkedin
-              icon="bx bxl-linkedin-square"
-              label="linkedin"
-              title="LinkedIn"
-              href="https://linkedin.com/in/armutcu"
-              newPage={true}
-            />
-            <Button // github
-              icon="bx bxl-github"
-              label="github"
-              title="Github"
-              href="https://github.com/thejayduck/"
-              newPage={true}
-            />
-            <Button // itchio
-              icon="bx bxs-store"
-              label="itchio"
-              title="Itch.io"
-              href="https://thejayduck.itch.io/"
-              newPage={true}
-            />
-            <Button // deviantart
-              icon="bx bxl-deviantart"
-              label="deviantart"
-              title="DeviantArt"
-              href="https://www.deviantart.com/thejayduck"
-              newPage={true}
-            />
-            <Button // cara
-              icon="bx bxs-copyright"
-              label="cara"
-              title="Cara"
-              href="https://cara.app/thejayduck"
-              newPage={true}
-            />
-            <Button // instagram
-              icon="bx bxl-instagram-alt"
-              label="instagram"
-              title="Instagram"
-              href="https://www.instagram.com/ardafevzi.armutcu/"
-              newPage={true}
-            />
+            <li>
+              <Button
+                icon="bx bx-link"
+                label="copy"
+                title="Copy Link to Page"
+                newPage={false}
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                  showToast(
+                    "Page Link Copied!",
+                    "The page link has been copied to your clipboard."
+                  );
+                }}
+              />
+            </li>
+            <hr />
+            {socialLinks.map((link) => (
+              <li key={link.label} className={styles.highlight}>
+                <Button
+                  icon={link.icon}
+                  label={link.label}
+                  title={link.title}
+                  href={link.href}
+                  newPage={true}
+                />
+                <Button
+                  icon="bx bx-link"
+                  label={`Copy link to ${link.label}`}
+                  onClick={() => {
+                    navigator.clipboard.writeText(link.href);
+                    showToast(
+                      "Link Copied!",
+                      `The link to "${link.title}" has been copied to your clipboard.`
+                    );
+                  }}
+                  newPage={false}
+                />
+              </li>
+            ))}
+            <hr />
+            <h3>Homepage Redirects</h3>
+
+            <li>
+              <Button // blog
+                icon="bx bxs-box"
+                label="blog"
+                title="Blog"
+                href="/blog"
+                newPage={false}
+              />
+            </li>
+            <li>
+              <Button // projects
+                icon="bx bxs-bulb"
+                label="projects"
+                title="Projects"
+                href="/projects"
+                newPage={false}
+              />
+            </li>
+            <li>
+              <Button // gallery
+                icon="bx bxs-image"
+                label="gallery"
+                title="Gallery"
+                href="/gallery"
+                newPage={false}
+              />
+            </li>
           </ul>
-          <span>This page is work in progress!</span>
         </section>
       </PageBase>
     </>
