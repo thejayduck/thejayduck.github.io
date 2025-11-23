e57ca702d0c390c17bb998d043af12d8c7343426
thejayduck <ardafevzi.armutcu@gmail.com>
2025-11-23 23:54:32 +0100

    Fixes database order in mongodb as well as in gallery.js

--- pages/api/gallery.js
+++ pages/api/gallery.js
@@ -5,7 +5,8 @@ import GalleryEntry from "@/lib/models/gallery";
 export default async function handler(req, res) {
   await dbConnect();
   if (req.method === "GET") {
-    const entries = await GalleryEntry.find({}).lean();
+    let entries = await GalleryEntry.find({}).lean();
+    entries = entries.reverse(); // Have to reverse because of how mongodb inserts new documents
     return res.status(200).json({ entries });
   }
 }
