65f60f4a8969ef43c9ed51806d5a8549f92630ad
thejayduck <ardafevzi.armutcu@gmail.com>
2025-12-16 10:47:37 +0100

    Fixes database sort issue

--- pages/api/gallery.js
+++ pages/api/gallery.js
@@ -5,8 +5,9 @@ import GalleryEntry from "@/lib/models/gallerySchema";
 export default async function handler(req, res) {
   await dbConnect();
   if (req.method === "GET") {
-    let entries = await GalleryEntry.find({ hidden: { $ne: true } }).lean();
-    entries = entries.reverse(); // Have to reverse because of how mongodb inserts new documents
+    let entries = await GalleryEntry.find({ hidden: { $ne: true } })
+      .sort({ _id: -1 })
+      .lean();
     return res.status(200).json({ entries });
   }
 }
